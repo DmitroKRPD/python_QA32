21cbf4379c29af8ecea9c20c7d7e38e9afc27ddd
User Office <user@kr.step>
2023-09-22 20:24:46 +0300

    Create

---
+++ pr/Lesson02/1.py
@@ -0,0 +1,40 @@
+# #import math #імпортую бібліотеку
+# #print(math.cos(2)) #
+
+# '''
+# from math import sin, cos, pi
+# print(pi)
+# '''
+
+# from math import sqrt as s, pi
+# print(pi)
+
+# findElementById = 5
+# k = s(9)
+# print(findElementById)
+# K = 5
+# print(type(k))
+# print(k)
+# k = 'grrewtryry'
+# print(type(k))
+# print(k+'12345'+k)
+
+# h = 4
+# w = 7
+# s = h*w
+
+
+# print(1,2,3,4,5,6,sep='sep',end='          ')
+# print(1,2,3,4,5,6)
+# print(1,2,3,4,5,6)
+
+
+# a = int(input('input a:'))
+# print(a+a)
+
+a = int(input('Input a:'))
+b = int(input('Input b:'))
+
+
+s = sum((a,b))
+print('sum = ',s)
